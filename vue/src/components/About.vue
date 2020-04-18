6332fbd00f5b076ecf0f90e1d2416413db73e31f
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2020-04-18 20:01:53 +0900

    prepared vue spa

---
+++ vue/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>ABOUT</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+Vue.use(Router)
+
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about/',
+      name: 'about',
+      component: About,
+    },
+  ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
