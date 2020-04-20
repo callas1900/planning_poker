936452e0c41d93964727dcfadc1834b4c07f026f
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2020-04-21 00:48:59 +0900

    making session id

--- vue/src/components/Make.vue
+++ vue/src/components/Make.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>Make</h1>
+    <h1>Make New Planning Session</h1>
+    <input v-model="owner" placeholder="input your name" />
+    <input type="button" name="make!" v-on:click="makeSession" />
+    <h1>{{code}}</h1>
   </div>
 </template>
 
@@ -9,13 +12,32 @@
 export default {
   name: "make",
   data() {
-    return {};
+    return {
+      owner: null,
+      code: null
+    };
   },
   mounted() {
     console.log(this.$database);
-    this.$database.once('value').then(function(dataSnapShot) {
+    this.$database.once("value").then(function(dataSnapShot) {
       console.log(dataSnapShot);
-    })
+    });
+  },
+  methods: {
+    makeSession: function() {
+      let uuid = this.generateUuid();
+      console.log(uuid);
+      this.code = uuid.split('-')[0];
+    },
+    generateUuid: function() {
+      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(
+        c
+      ) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
+    }
   }
 };
 </script>
