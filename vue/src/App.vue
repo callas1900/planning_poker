53235d144d52f13e4ae265c83e83f8cd9238aa72
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2020-04-22 10:53:11 +0900

    add explanation at about page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -39,11 +39,6 @@ ul {
   padding: 0;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
 a {
   color: #42b983;
 }
--- vue/src/components/About.vue
+++ vue/src/components/About.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <h1>ABOUT</h1>
+    <h2>How to play</h2>
+    <h3>Starting Game</h3>
+    <ul>
+      <li>1. PO makes a session for planning. then PO provide a session code.</li>
+      <li>2. Member join to the session by the code.</li>
+    </ul>
+    <h3>In Game</h3>
+    <ul>
+      <li>* Each member can decide own number.</li>
+      <li>* After all of members decided it, A result scre will be shown up.</li>
+      <li>* The result is sorted numbers with member's name.</li>
+      <li>* PO can reset the game.</li>
+    </ul>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: "home",
+  name: "about",
   data() {
     return {};
   }
@@ -15,4 +28,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ul{
+
+}
 </style>
